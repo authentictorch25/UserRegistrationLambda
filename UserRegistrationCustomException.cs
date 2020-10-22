dc0c14b6d593e5d240347f1ac8e757280cd0e513
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-23 01:59:01 +0530

    [Akash]User registration problrm using lambda expression

---
+++ UserRegistrationCustomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistrationProgram
+{
+   public  class UserRegistrationCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_DETAILS
+        }
+        public readonly ExceptionType type;
+
+        /// <summary>
+        /// UC 12 : Created Custom Exception For User Reg Problem
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="message"></param>
+        public UserRegistrationCustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
